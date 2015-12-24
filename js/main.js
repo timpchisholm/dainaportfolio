199aa7a39eadbf44e7c78b5be80d3a3185bbc4e1
Tim Chisholm <timpchisholm@Tims-MacBook-Pro.local>
2015-12-23 21:24:48 -0500

    added more background images to the social icons and added fluidbox to img in midsummers

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-var socialhover = function() {
-	$('.fb').addClass('hide');
-	$('.fb-hover').removeClass('hide');
-}
 
-var socialreturn = function() {
-	$('.fb-hover').addClass('hide');
-	$('.fb').removeClass('hide');
-}
-
-$('.fb').mouseenter(socialhover)
-
-$('.fb-hover').mouseleave(socialreturn)
-
+$('.costume-description a').fluidbox();
 
 
 
