0236c733c622af0303fed86d3e6276f5cd7a78db
Tim Chisholm <timpchisholm@Tims-MacBook-Pro.local>
2015-12-27 14:17:19 -0500

    finished styling the social links and redid the nav menu away from white highlight. Attempted to turn costume-nav into aninamted element but no dice. Work in progress

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 
 $('.costume-description a').fluidbox();
 
+// $( ".navhere" ).hover(
+//   function() {
+//     $( '.costumenav' ).slideDown();
+//   }, function() {
+//     $( '.costumenav' ).slideUp();
+//   }
+// );
 
+$(".navhere").click(function(){
+	$('.costumenav').slideDown();
 
-
+})
