a06fb8dd44b1ebd64ded5a8645e2981661bba35c
Tim Chisholm <timpchisholm@Tims-MacBook-Pro.local>
2016-01-08 16:50:47 -0500

    rewrote the thumbnail galleries, redrew the arrows, removed the css nth-child styling, added js to animate galleries. Oddly, will only display maximum of five gallery images

--- js/main.js
+++ js/main.js
@@ -1,33 +1,62 @@
-
 $('.costume-description a').fluidbox();
 $('.thumb a').fluidbox();
 
-// $( ".navhere" ).hover(
-//   function() {
-//     $( '.costumenav' ).slideDown();
-//   }, function() {
-//     $( '.costumenav' ).slideUp();
-//   }
-// );
-
-// $('li .navhere').on('click', function(){
-// 	$('.costumesnav').slideDown();
-// })
-
-// $(document).ready(function(){
-// 	$(".navhere").hover
-// 		(function(){
-// 			$('.costumesnav').css('display', 'block');
-// 	},
-// 		(function(){
-// 			$('costumesnav').css('display', 'none');
-// 		});
-
-// });
 
 
 $('#costume-drop').on('click', function() {
-
-	$('.costumesnav').slideToggle(100);
-
+		if (window.innerWidth <= 1023){
+			$('.costumesnav').slideToggle(100);
+		}
 });
+
+
+$(document).ready(function(){
+
+  // On click of the NEXT button
+  $('.next').on('click', function(){
+    if ( $('.shown').is(':last-child') ) {
+      jQuery.noop();
+    }
+    else {
+      $('.shown').removeClass('shown').next().addClass('shown');
+      $('.costume-description a').fluidbox();
+	  $('.thumb a').fluidbox();
+    }
+  });
+
+  // On click of the BACK button
+  $('.back').on('click', function(){
+
+    if ( $('.shown').is(':first-child') ) {
+		jQuery.noop();
+    }
+    else {
+      $('.shown').removeClass('shown').prev().addClass('shown');
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
